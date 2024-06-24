474692fedd026961e4b2c483a61958f2e7eece37
Shahrzadzhr <sh.zahrayi@gmail.com>
2024-06-24 16:14:06 +0200

    first commit

--- lib/src/features/gold/presentation/gold_screen.dart
+++ lib/src/features/gold/presentation/gold_screen.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:stream_gold_rate/fake_gold_api.dart';
 
 class GoldScreen extends StatelessWidget {
   const GoldScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    /// Platzhalter für den Goldpreis
-    /// soll durch den Stream `getGoldPriceStream()` ersetzt werden
-    const double goldPrice = 69.22;
-
     return SafeArea(
       child: Scaffold(
         body: Padding(
@@ -21,14 +18,30 @@ class GoldScreen extends StatelessWidget {
               Text('Live Kurs:',
                   style: Theme.of(context).textTheme.headlineMedium),
               const SizedBox(height: 20),
-              // TODO: Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
-              // statt des konstanten Platzhalters
-              Text(
-                NumberFormat.simpleCurrency(locale: 'de_DE').format(goldPrice),
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineLarge!
-                    .copyWith(color: Theme.of(context).colorScheme.primary),
+              StreamBuilder<double>(
+                stream: FakeGoldApi.getGoldPriceStream(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}',
+                        style:
+                            const TextStyle(color: Colors.red, fontSize: 18));
+                  } else if (snapshot.hasData) {
+                    return Text(
+                      NumberFormat.simpleCurrency(locale: 'de_DE')
+                          .format(snapshot.data),
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineLarge!
+                          .copyWith(
+                              color: Theme.of(context).colorScheme.primary),
+                    );
+                  } else {
+                    return const Text('Daten nicht verfügbar',
+                        style: TextStyle(color: Colors.grey, fontSize: 18));
+                  }
+                },
               ),
             ],
           ),
